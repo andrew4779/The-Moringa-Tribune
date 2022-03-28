b00fc17d22a6003ec3e27c27883a501533d211fc
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-03-28 10:29:54 +0300

    Made final changes

---
+++ news/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.conf import settings
+from . import views
+from django.conf.urls.static import static
+
+urlpatterns=[
+
+   path('',views.news_today, name='newsToday'),
+   path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews'),
+   path('',views.search_results, name='search_results'),
+   path('',views.article,name ='article')
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
