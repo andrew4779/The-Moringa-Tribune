91b7ec459c04c64f89ed4089215d7b1b781c798e
andrew4779 <andrewkimani02@gmail.com>
2023-03-09 12:00:37 +0300

    new changes

--- tribune/settings.py
+++ tribune/settings.py
@@ -156,4 +156,4 @@ EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 # Configure Django App for Heroku.
-# django_heroku.settings(locals())
+# django_heroku.settings(locals(uhuh))
